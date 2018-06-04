83f9aa8e6280a4054f8d94ebbb72fc65eb1c6b5c
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-06-04 17:52:38 +0100

    Start weekend filter

--- tl_shift_pattern.rb
+++ tl_shift_pattern.rb
@@ -5,10 +5,31 @@ def create_shift #creating a shift
   new_shift = ShiftGen.new.shifts
 end
 
-def tl_filter(shift)
+def tl_filter(shifts) #We'll pass create_shift
 
   #At least 2 weekends off
-  shift
+  #First day Sunday last day Sat
+  #We need index 0 and 6 be off in two consecutive
+  #weeks
+  shifts
+  counter = 0
+  for weeks in shifts
+    if counter == 4
+      return true
+    end
+
+    if counter != 2
+      counter = 0
+    end
+
+    for days in weeks
+
+      if shifts[weeks][0] || shifts[weeks][6] == "O"
+          counter += 1
+
+      end
+    end
+  end
 
   #starting with a late
   #finishing with early shift?
